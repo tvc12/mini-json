c813ead294b55fc2b7662d2be0435ceb1692c6a9
Thien Chi Vi <meomeocf98@gmail.com>
2020-07-19 23:13:49 +0700

    😍 Support local config (#7)
    * support: local config
    
    * export: transform key

--- src/index.ts
+++ src/index.ts
@@ -14,31 +14,33 @@ const snakeCase = (value: string): string => {
   return value.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
 };
 
-abstract class MiniJson {
-  private static serializeKeyTransform: (key: string) => string = snakeCase;
-  private static deserializeKeyTransform: (key: string) => string = camelCase;
+type KeyTransform = (key: string) => string;
 
-  static serializeKeysTo(transform: (key: string) => string): void {
+abstract class MiniJson {
+  private static serializeKeyTransform: KeyTransform = snakeCase;
+  private static deserializeKeyTransform: KeyTransform = camelCase;
+
+  static serializeKeysTo(transform: KeyTransform): void {
     this.serializeKeyTransform = transform;
   }
 
-  static deserializeKeysFrom(transform: (key: string) => string): void {
+  static deserializeKeysFrom(transform: KeyTransform): void {
     this.deserializeKeyTransform = transform;
   }
 
   private static convertKeys<T>(
     data: any,
-    transform: (key: string) => string
+    keyTransform: (key: string) => string
   ): T {
     if (isArray(data)) {
       return data.map((innerObj: any) =>
-        this.convertKeys<any>(innerObj, transform)
+        this.convertKeys<any>(innerObj, keyTransform)
       ) as any;
     } else if (isObject(data)) {
       const newObj: { [key: string]: any } = {};
       Object.entries(data).forEach(([key, value]) => {
-        const newKey = transform(key);
-        newObj[newKey] = this.convertKeys(value, transform);
+        const newKey = keyTransform(key);
+        newObj[newKey] = this.convertKeys(value, keyTransform);
       });
       return newObj as any;
     } else {
@@ -46,32 +48,22 @@ abstract class MiniJson {
     }
   }
 
-  static toJson(value: any): string {
-    return JSON.stringify(this.serializerKeys(value));
+  static toJson(value: any, keyTransform?: KeyTransform): string {
+    const transform = keyTransform || this.serializeKeyTransform;
+    return JSON.stringify(this.convertKeys<any>(value, transform));
   }
 
-  private static serializerKeys(value?: any): any {
-    if (value) {
-      return this.convertKeys<any>(value, this.serializeKeyTransform);
-    }
-  }
+  static fromJson<T>(data: string | object, keyTransform?: KeyTransform): T {
+    const transform = keyTransform || this.deserializeKeyTransform;
 
-  static fromJson<T>(data: string | object): T {
     if (typeof data === 'string') {
       const json = JSON.parse(data);
-      return this.toObject(json);
+      return this.convertKeys<T>(json, transform);
     } else {
-      return this.toObject(data);
-    }
-  }
-
-  private static toObject<T>(data: any): T {
-    if (data) {
-      return this.convertKeys<T>(data, this.deserializeKeyTransform);
-    } else {
-      throw new Error("Can't from json");
+      return this.convertKeys<T>(data, transform);
     }
   }
 }
 
+export { KeyTransform };
 export default MiniJson;
--- tests/mini_json.test.ts
+++ tests/mini_json.test.ts
@@ -1,5 +1,6 @@
 import MiniJson from '../src/index';
 import { expect } from 'chai';
+import { camelCase, kebabCase, snakeCase } from 'lodash';
 
 describe('Mini json test', () => {
   let obj: any;
@@ -20,15 +21,84 @@ describe('Mini json test', () => {
       }
     `;
     obj = MiniJson.fromJson<any>(json);
-    expect(obj).is.not.null;
-    expect(obj.canLoadMore).is.not.null;
-    expect(obj.data).is.not.empty;
-    expect(obj.data[0].dayOfBirthday).is.not.null;
+    expect(obj).to.not.be.null;
+    expect(obj.canLoadMore).is.not.be.null;
+    expect(obj.data).to.not.be.empty;
+    expect(obj.data[0].dayOfBirthday).to.not.be.null;
     console.log('data::', obj);
   });
 
   it('Object to json success', () => {
     const json = MiniJson.toJson(obj);
+    expect(json).to.not.null;
+    console.log('json', json);
+  });
+});
+
+describe('Custom global naming convention', () => {
+  MiniJson.serializeKeysTo(camelCase);
+  MiniJson.deserializeKeysFrom(kebabCase);
+
+  let obj: any;
+  it('Parse json to object success with kebabCase', () => {
+    const json = `{
+      "success": true,
+      "can_load_more": true,
+      "data": [
+        {
+          "day_of_birthday": "16/10/2020",
+          "ids": [1, 2, 3]
+        },
+        {
+          "day_of_birthday": "17/10/2020",
+          "ids": [4, 5, 6]
+        }
+      ]
+      }
+    `;
+    obj = MiniJson.fromJson<any>(json);
+    expect(obj).to.not.be.null;
+    expect(obj['can-load-more']).is.not.be.null;
+    expect(obj.data).to.not.be.empty;
+    expect(obj.data[0]['day-of-birthday']).to.not.be.null;
+    console.log('data::', obj);
+  });
+
+  it('Object to json success with camelCase', () => {
+    const json = MiniJson.toJson(obj);
+    expect(json).is.not.null;
+    console.log('json', json);
+  });
+});
+
+describe('Custom local naming convention', () => {
+  let obj: any;
+  it('Parse json to object success with kebabCase', () => {
+    const json = `{
+      "success": true,
+      "can_load_more": true,
+      "data": [
+        {
+          "day_of_birthday": "16/10/2020",
+          "ids": [1, 2, 3]
+        },
+        {
+          "day_of_birthday": "17/10/2020",
+          "ids": [4, 5, 6]
+        }
+      ]
+      }
+    `;
+    obj = MiniJson.fromJson<any>(json, kebabCase);
+    expect(obj).to.not.be.null;
+    expect(obj['can-load-more']).is.not.be.null;
+    expect(obj.data).to.not.be.empty;
+    expect(obj.data[0]['day-of-birthday']).to.not.be.null;
+    console.log('data::', obj);
+  });
+
+  it('Object to json success with snakeCase', () => {
+    const json = MiniJson.toJson(obj, snakeCase);
     expect(json).is.not.null;
     console.log('json', json);
   });

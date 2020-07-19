8e3860b0387bdac9db4b6b6b189c447032759faf
tvc12 - Thien Vi Chi <meomeocf98@gmail.com>
2020-07-19 23:09:25 +0700

    support: local config

--- src/index.ts
+++ src/index.ts
@@ -14,31 +14,33 @@ const snakeCase = (value: string): string => {
   return value.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
 };
 
-abstract class MiniJson {
-  private static serializeKeyTransform: (key: string) => string = snakeCase;
-  private static deserializeKeyTransform: (key: string) => string = camelCase;
+type TransformKey = (key: string) => string;
 
-  static serializeKeysTo(transform: (key: string) => string): void {
+abstract class MiniJson {
+  private static serializeKeyTransform: TransformKey = snakeCase;
+  private static deserializeKeyTransform: TransformKey = camelCase;
+
+  static serializeKeysTo(transform: TransformKey): void {
     this.serializeKeyTransform = transform;
   }
 
-  static deserializeKeysFrom(transform: (key: string) => string): void {
+  static deserializeKeysFrom(transform: TransformKey): void {
     this.deserializeKeyTransform = transform;
   }
 
   private static convertKeys<T>(
     data: any,
-    transform: (key: string) => string
+    keyTransform: (key: string) => string
   ): T {
     if (isArray(data)) {
       return data.map((innerObj: any) =>
-        this.convertKeys<any>(innerObj, transform)
+        this.convertKeys<any>(innerObj, keyTransform)
       ) as any;
     } else if (isObject(data)) {
       const newObj: { [key: string]: any } = {};
       Object.entries(data).forEach(([key, value]) => {
-        const newKey = transform(key);
-        newObj[newKey] = this.convertKeys(value, transform);
+        const newKey = keyTransform(key);
+        newObj[newKey] = this.convertKeys(value, keyTransform);
       });
       return newObj as any;
     } else {
@@ -46,30 +48,25 @@ abstract class MiniJson {
     }
   }
 
-  static toJson(value: any): string {
-    return JSON.stringify(this.serializerKeys(value));
+  static toJson(
+    value: any,
+    keyTransform?: TransformKey
+  ): string {
+    const transform = keyTransform || this.serializeKeyTransform;
+    return JSON.stringify(this.convertKeys<any>(value, transform));
   }
 
-  private static serializerKeys(value?: any): any {
-    if (value) {
-      return this.convertKeys<any>(value, this.serializeKeyTransform);
-    }
-  }
+  static fromJson<T>(
+    data: string | object,
+    keyTransform?: TransformKey
+  ): T {
+    const transform = keyTransform || this.deserializeKeyTransform;
 
-  static fromJson<T>(data: string | object): T {
     if (typeof data === 'string') {
       const json = JSON.parse(data);
-      return this.toObject(json);
+      return this.convertKeys<T>(json, transform);
     } else {
-      return this.toObject(data);
-    }
-  }
-
-  private static toObject<T>(data: any): T {
-    if (data) {
-      return this.convertKeys<T>(data, this.deserializeKeyTransform);
-    } else {
-      throw new Error("Can't from json");
+      return this.convertKeys<T>(data, transform);
     }
   }
 }
